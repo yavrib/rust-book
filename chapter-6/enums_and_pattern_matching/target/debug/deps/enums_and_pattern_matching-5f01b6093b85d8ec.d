53bc8de4efaee8cc56b2e4c83e3570a34aa980b6
Berkan Yavri <yavrib@gmail.com>
2018-09-30 21:58:39 +0200

    Remove target from chapter-6

--- chapter-6/enums_and_pattern_matching/target/debug/deps/enums_and_pattern_matching-5f01b6093b85d8ec.d
+++
@@ -1,5 +0,0 @@
-/home/archeios/Projects/rust-book/chapter-6/enums_and_pattern_matching/target/debug/deps/enums_and_pattern_matching-5f01b6093b85d8ec: src/main.rs
-
-/home/archeios/Projects/rust-book/chapter-6/enums_and_pattern_matching/target/debug/deps/enums_and_pattern_matching-5f01b6093b85d8ec.d: src/main.rs
-
-src/main.rs:
